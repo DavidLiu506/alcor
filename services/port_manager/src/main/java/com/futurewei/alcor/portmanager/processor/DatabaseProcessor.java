f707cffc768b755e5625027324dd450273ba0924
Dahai Liu <dliu@futurewei.com>
2021-08-23 18:50:57 -0700

    Revert "Update code"
    This reverts commit 8ed41107

--- services/port_manager/src/main/java/com/futurewei/alcor/portmanager/processor/DatabaseProcessor.java
+++ services/port_manager/src/main/java/com/futurewei/alcor/portmanager/processor/DatabaseProcessor.java
@@ -21,7 +21,7 @@ import com.futurewei.alcor.web.entity.port.PortEntity;
 
 import java.util.*;
 
-@AfterProcessor(PortProcessor.class)
+@AfterProcessor(DataPlaneProcessor.class)
 public class DatabaseProcessor extends AbstractProcessor {
     private List<NeighborInfo> buildNeighborInfos(InternalPortEntity internalPortEntity) {
         List<NeighborInfo> neighborInfos = new ArrayList<>();
