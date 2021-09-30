f37801c04f1c5a9994981d2d2fbc3ed6883e7601
Dahai Liu <dliu@futurewei.com>
2021-09-29 18:22:04 -0700

    Update code

--- lib/src/main/java/com/futurewei/alcor/common/executor/ThreadPoolExecutorConfig.java
+++ lib/src/main/java/com/futurewei/alcor/common/executor/ThreadPoolExecutorConfig.java
@@ -18,10 +18,10 @@ package com.futurewei.alcor.common.executor;
 
 public class ThreadPoolExecutorConfig {
     //Core thread pool size
-    public static int corePoolSize = 32;
+    public static int corePoolSize = 200;
 
     //Maximum thread pool size
-    public static int maximumPoolSize = 128;
+    public static int maximumPoolSize = 500;
 
     //Maximum idle time of thread
     public static int KeepAliveTime = 5000;
